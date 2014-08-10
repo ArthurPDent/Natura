1d8d278913f89a341438582da5d84eb66691596f
ArthurPDent <jackthias@pummelhead.com>
2014-08-09 23:04:46 -0700

    Update CloudMaterial.java
    Reverting change; needs to be made within block class for clouds, not the material class

--- src/main/java/mods/natura/CloudMaterial.java
+++ src/main/java/mods/natura/CloudMaterial.java
@@ -22,15 +22,6 @@ public class CloudMaterial extends Material
     }
 
     /**
-     * Set harvest tool
-     */
-    @Override
-    public boolean requiresTool ()
-    {
-        return true;
-    }
-    
-    /**
      * Returns if this material is considered solid or not
      */
     @Override
